3664f9ff93150d22480eb1877bf80d5e773eee3c
andreatpr <andreatapiapr@gmail.com>
2024-09-13 15:32:42 -0500

    feat(backend): crud doctor implementation

--- src/main/java/com/hampcode/repository/DoctorRepository.java
+++ src/main/java/com/hampcode/repository/DoctorRepository.java
@@ -1,4 +1,7 @@
 package com.hampcode.repository;
 
-public interface DoctorRepository {
+import com.hampcode.model.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 }
