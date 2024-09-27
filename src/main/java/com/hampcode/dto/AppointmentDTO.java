9c4c8853fa5080248897523c31c9d6b54c9e45b6
Paula215 <95729131+Paula215@users.noreply.github.com>
2024-09-26 22:12:45 -0500

    Update AppointmentDTO.java

--- src/main/java/com/hampcode/dto/AppointmentDTO.java
+++ src/main/java/com/hampcode/dto/AppointmentDTO.java
@@ -1,15 +1,17 @@
 package com.hampcode.dto;
-import lombok.AllArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+
 @Data
-@AllArgsConstructor
+
 public class AppointmentDTO {
-    private String doctorName;
-    private LocalDate date;
-    private LocalTime time;
-    private Boolean reserved;
-    private String type;
+    private Integer id;
+    @NotBlank(message = "Reason is mandatory")
+    private String reason;
+    @NotNull(message = "Availability is mandatory")
+    private AvailabilityDetailsDTO availability;
+
 }
