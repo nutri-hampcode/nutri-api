f1c9dba3343ea9b2a70ac634ff226fbe9655ba53
Paula215 <95729131+Paula215@users.noreply.github.com>
2024-09-26 22:16:22 -0500

    Update AppointmentService.java

--- src/main/java/com/hampcode/service/AppointmentService.java
+++ src/main/java/com/hampcode/service/AppointmentService.java
@@ -1,13 +1,14 @@
 package com.hampcode.service;
 
-import com.hampcode.model.entity.Appointment;
+import com.hampcode.dto.AppointmentDTO;
+import com.hampcode.model.entity.History;
 
 import java.util.List;
 
 public interface AppointmentService {
-    List<Appointment> findAll();
-    Appointment getOne(Integer id);
-    Appointment create(Appointment appointment);
-    Appointment update(Integer id, Appointment appointment);
+    List<AppointmentDTO> findAll();
+    AppointmentDTO getOne(Integer id);
+    AppointmentDTO create(Integer historyId, AppointmentDTO appointment);
+    AppointmentDTO update(Integer id, AppointmentDTO appointment);
     void delete(Integer id);
 }
