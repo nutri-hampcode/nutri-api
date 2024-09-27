9c6f7255d731e7ce98398d3ababfcdde3be5d5e7
Paula215 <95729131+Paula215@users.noreply.github.com>
2024-09-26 22:14:50 -0500

    Update AppointmentRepository.java

--- src/main/java/com/hampcode/repository/AppointmentRepository.java
+++ src/main/java/com/hampcode/repository/AppointmentRepository.java
@@ -2,8 +2,15 @@ package com.hampcode.repository;
 
 import com.hampcode.model.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    @Query("SELECT a from Appointment a where a.availability.id = :id_availability")
+    Optional<List<Appointment>> findByAvailability(@Param("id_availability") Integer id_availability);
 }
